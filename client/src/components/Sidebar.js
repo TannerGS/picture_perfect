cd2f3eb4b810485ffc925f2bc10ebe216d6f91ae
tanner <tannersimpkins@gmail.com>
2022-02-20 18:40:32 -0600

    Fixed bug

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom';
 import { RiHomeFill } from 'react-icons/ri';
-import { IoIOsArrowForward } from 'react-icons/io';
 
 import { categories } from '../utils/data';
 
